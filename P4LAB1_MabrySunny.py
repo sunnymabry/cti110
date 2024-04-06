75e4c6c0e447b7780c4144d8fce3fcd6e2d61c1d
sunnymabry <146143832+sunnymabry@users.noreply.github.com>
2024-04-06 09:50:53 -0400

    Add files via upload

---
+++ P4LAB1_MabrySunny.py
@@ -0,0 +1,11 @@
+import turtle
+turtle.showturtle()
+turtle.shape("turtle")
+for x in range(4):
+    turtle.forward(50)
+    turtle.right(90)
+length = 100
+angle = 120
+for i in range(3):
+    turtle.forward(length)
+    turtle.right(angle)
